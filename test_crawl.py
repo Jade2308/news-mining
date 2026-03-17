2a9a35c06201cc8dc6f5e27ba94f515cda541407
Jade2308 <vinhsanh2308@gmail.com>
2026-03-17 22:22:25 +0700

    fix module 1

--- test_crawl.py
+++ test_crawl.py
@@ -1,11 +1,12 @@
-# test_crawl.py
 from crawlers.vnexpress_crawler import VNExpressCrawler
+from database.schema import init_db
 from database.db import insert_article
+
+init_db()  # ✅ tạo bảng articles nếu chưa có
 
 crawler = VNExpressCrawler(category='kinh-doanh')
 articles = crawler.run()
 
-# Lưu vào DB
 for article in articles:
     insert_article(article)
 
